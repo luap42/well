9829013be6a3d5773ca26821f5d71145c3d3b295
luap42 <pstrobach15+luap42@gmail.com>
2024-10-05 00:59:21 +0200

    Allow searching participants

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,6 +2,18 @@ class ParticipantsController < ApplicationController
   before_action :get_case, only: [ :index ]
 
   def index
+    @participants = @case.participants
+
+    unless params[:name].blank?
+      @participants = @participants.where("name LIKE ?", "%" + Participant.sanitize_sql_like(params[:name]) + "%")
+    end
+
+    unless params[:participant_role].blank?
+      @participants = @participants.where(participant_role: ParticipantRole.find(params[:participant_role]))
+    end
+
+    @participants = @participants.order(:participant_role_id)
+
     render layout: "layouts/case_view"
   end
 end
