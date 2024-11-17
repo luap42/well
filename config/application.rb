3fe0d044ec37e43f5eaa5ae3f0bbd62cb9c3a652
luap42 <pstrobach15+luap42@gmail.com>
2024-11-17 21:09:23 +0100

    Change default timezone

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,9 @@ module Well
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.time_zone = "Europe/Berlin"
     config.i18n.default_locale = :de
   end
 end
