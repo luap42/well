6788c929b0502e73c779dde14508a265ece77394
luap42 <pstrobach15+luap42@gmail.com>
2024-12-14 02:09:50 +0100

    More tweaks, add infos to task col header

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,6 +8,9 @@ class Task < ApplicationRecord
 
   default_scope { where.not(is_deleted: true) }
 
+  scope :that_are_open, -> { where(is_resolved: false) }
+  scope :that_are_overdue, -> { where("due < ?", Date.today) }
+
   def status
     return :deleted if is_deleted
     return :resolved if is_resolved
