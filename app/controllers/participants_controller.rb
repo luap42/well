062ce32a8ba2a8594d9b6c6b2292effd6d58ee06
luap42 <pstrobach15+luap42@gmail.com>
2025-01-12 20:36:31 +0100

    Implement participants permissions

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -3,6 +3,7 @@ class ParticipantsController < ApplicationController
   before_action :get_participant, only: [ :edit, :update ]
 
   def index
+    return if require_permission! :participants_read
     @participants = @case.participants
 
     unless params[:name].blank?
@@ -19,10 +20,12 @@ class ParticipantsController < ApplicationController
   end
 
   def edit
+    return if require_permission! :participants_write
     render layout: "layouts/case_view"
   end
 
   def update
+    return if require_permission! :participants_write
     role = ParticipantRole.find(params[:participant][:participant_role])
 
     @participant.update!(
@@ -46,11 +49,13 @@ class ParticipantsController < ApplicationController
   end
 
   def new
+    return if require_permission! :participants_write
     @participant = Participant.new(case: @case)
     render layout: "layouts/case_view"
   end
 
   def create
+    return if require_permission! :participants_write
     role = ParticipantRole.find(params[:participant][:participant_role])
 
     @participant = Participant.create!(
