bfa0adce0198afcff3dc959896e7d5cebc9f30b4
luap42 <pstrobach15+luap42@gmail.com>
2025-01-17 16:40:32 +0100

    Display summary of times per case

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # convert time in minutes to fully qualified time frame
+  def min_to_fqtf(time)
+    min = time % 60
+    h = (time / 60).floor % 24
+    d = (time / 24 / 60).floor
+
+    resp = "#{min}min"
+    resp = "#{h}h #{resp}" if h != 0
+    resp = "#{h}d #{resp}" if d != 0
+
+    resp
+  end
 end
