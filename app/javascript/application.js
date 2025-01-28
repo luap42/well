1986a40faf9f3a229f5d80ee234ab2efb07913b7
luap42 <pstrobach15+luap42@gmail.com>
2025-01-28 13:45:26 +0100

    Add some more trix options

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,36 @@ import "./popper"
 import "./bootstrap"
 import "trix"
 import "@rails/actiontext"
+
+
+/* what the newly created button does */
+Trix.config.textAttributes.highlighted = {
+    tagName: 'mark'
+}
+
+Trix.config.textAttributes.h2 = {
+    tagName: 'h2'
+}
+
+Trix.config.textAttributes.underlined = {
+    tagName: 'u'
+}
+
+/* insert the button visual in the default toolbar */
+addEventListener("trix-initialize", function(event) {
+    var highlightedButtonHTML = '<button type="button" class="trix-button" data-trix-attribute="highlighted">Mark</button>'
+    var h2ButtonHTML = '<button type="button" class="trix-button" data-trix-attribute="h2">H2</button>'
+    var underlinedButtonHTML = '<button type="button" class="trix-button" data-trix-attribute="underlined">Unter</button>'
+
+    event.target.toolbarElement.
+    querySelector(".trix-button-group").
+    insertAdjacentHTML("beforeend", highlightedButtonHTML)
+
+    event.target.toolbarElement.
+    querySelector(".trix-button-group").
+    insertAdjacentHTML("beforeend", h2ButtonHTML)
+
+    event.target.toolbarElement.
+    querySelector(".trix-button-group").
+    insertAdjacentHTML("beforeend", underlinedButtonHTML)
+})
